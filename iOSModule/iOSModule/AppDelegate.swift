316a351c74a69ea6d371365934980f63e03148c0
see <upcwangying@126.com>
2022-06-04 12:03:50 +0800

    iOSModule

---
+++ iOSModule/iOSModule/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  iOSModule
+//
+//  Created by Ying Wang on 2022/6/4.
+//
+
+import UIKit
+import Flutter
+import FlutterPluginRegistrant
+
+@UIApplicationMain
+class AppDelegate: FlutterAppDelegate {
+
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let flutterEngine = FlutterEngine(name: "my flutter engine")
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController(engine: flutterEngine)
+
+        window.rootViewController = vc
+        self.window = window
+        return true
+    }
+
+
+}
+
